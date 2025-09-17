b5168bb0a2169377c93592bc9edcc71d2693efed
Serhii Kermas <serhii.kermas@gmail.com>
2025-09-17 17:08:09 +0300

    Solution-edits-1

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# Install Kind
+kind create cluster --config cluster.yml
+
 # Install Ingress Controller
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
 
-# Install and Deploy Helm Chart
-helm install todoapp
+# Deploy Helm Chart an Dependencies
 helm upgrade todoapp helm-chart/todoapp -n default
+helm dependency build
